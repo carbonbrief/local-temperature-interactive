ab83f7a8b1156649a148a0a2ac1afdfca2fde7b4
Rosamund Pearce <rosamundpearce@live.com>
2018-06-21 13:24:30 +0100

    make grid style zoom-dependent :sparkles:

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function() {
+
     map.addLayer({
         id: 'tiles',
         type: 'fill',
@@ -37,17 +38,70 @@ map.on('load', function() {
         },
         paint: {
             'fill-color': "#f3f3f3",
-            'fill-opacity': 0.1
+            'fill-opacity': 0.5
         },
-        // 'fill-outline': '#f3f3f3',
-        // 'fill-outline-opacity': 0.5
-
+        filter: [ 'all',
+            [ '==', 'GEOID10', 'NONE' ] // start with a filter that doesn't select anything
+        ]
     });
 
-    // map.on('mouseenter', 'tiles', function(e) {
-    //     map.setPaintProperty('tiles', 'fill-opacity' {
+    map.addLayer({
+        id: 'outline',
+        type: 'line',
+        source: {
+            type: 'geojson',
+            data: './data/tiles.geojson'
+        },
+        paint: {
+            'line-color': '#f3f3f3',
+            'line-opacity': {
+                "type": "exponential",
+                "stops": [
+                    [1.5,0],
+                    [3,0.2],
+                    [5,0.4],
+                ]
+            },
+            'line-width': {
+                "type": "exponential",
+                "stops": [
+                    [1.5,0],
+                    [3,0.5],
+                    [5,0.7],
+                    [7,0.9]
+                ]
+            }
+        }
+    });
 
+    // map.on('mousemove', function (e) {
+
+    //     // query the map for the under the mouse
+    //     map.featuresAt(e.point, { radius: 5, includeGeometry: true }, function (err, features) {
+    //         if (err) throw err
+    //         console.log(e.point, features)
+    //         var ids = features.map(function (feat) { return feat.properties.GEOID10 })
+
+    //         // set the filter on the hover style layer to only select the features
+    //         // currently under the mouse
+    //         map.setFilter('states-hover', [ 'all',
+    //         [ 'in', 'GEOID10' ].concat(ids)
+    //         ])
     //     })
+
+    // });
+
+    // map.on('mouseenter', 'tiles', function(e) {
+    //     map.setPaintProperty('tiles', {
+    //         'fill-opacity': 0.8
+    //     });
     // })
+
+    // polygonLayer.on('mouseover', function (this) {
+    //     this.setStyle({
+    //         fillOpacity: 0,
+    //         color: 'black'
+    //         });
+    // });
 
 });
