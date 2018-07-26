6c656d7257a041cbe5ad1d291b4928bf7a655553
Rosamund Pearce <rosamundpearce@live.com>
2018-07-26 16:18:53 +0100

    fixed :bug: with second share button

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-// first attempt - working, but want to open within the same window
+// first attempt - leaving for now as may want to use some of this
 
 // function tweetIt () {
 
@@ -13,39 +13,24 @@
 //     window.open(tweetUrl);
 // }
 
-// second attempt
-
-// update when map clicked
-
-// $('#map').on('click', function(ev) {
-//     ev.preventDefault();
-//     // Remove existing iframe
-//     $('#tweetBtn iframe').remove();
-//     // Generate new markup
-//     var tweetBtn = $('<a></a>')
-//         .addClass('twitter-share-button')
-//         .attr('href', 'http://twitter.com/share')
-//         .attr('data-url', 'http://test.com')
-//         .attr('data-via', 'CarbonBrief')
-//         .attr('data-text', $('#historic').text());
-//     $('#tweetBtn').append(tweetBtn);
-//     twttr.widgets.load();
-//     // twttr.widgets.onload(changeColor);
-
-//     // function changeColor () {
-//     //     $('.btn').css("background-color", "#333333");
-//     //     console.log("change color");
-//     // }
-// });
-
 // third attempt
 
 // Remove existing iframe
 $('#tweet iframe').remove();
 
-    //For custom button
-    var baseTweet = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fexample.com&text=";
-    var newText = 'Here is a new Quote';
-    var encoded = encodeURI(newText);
-    var customTweet = baseTweet += encoded;
-    $('#custom > a').attr('href', customTweet);
+// For first custom button
+
+var baseTweet = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fexample.com&text=";
+var newText = document.getElementById('historic').innerText;
+var encoded = encodeURI(newText);
+var customTweet = baseTweet += encoded;
+$('#custom > a').attr('href', customTweet);
+
+// For second custom button
+
+var baseTweet2 = "https://twitter.com/intent/tweet?url=http%3A%2F%2Fexample.com&text=";
+var newText2 = document.getElementById('future').innerText;
+var encoded2 = encodeURI(newText2);
+console.log(encoded2);
+var customTweet2 = baseTweet2 += encoded2;
+$('#custom2 > a').attr('href', customTweet2);
