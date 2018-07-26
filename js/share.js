55a2539903f59ae5504b8f0c72df99a5d255b473
Rosamund Pearce <rosamundpearce@live.com>
2018-07-26 14:42:20 +0100

    Tweet button with popup
    Although dynamic text still not working

--- js/share.js
+++ js/share.js
@@ -1,3 +1,5 @@
+// first attempt - working, but want to open within the same window
+
 function tweetIt () {
 
     var phrase = document.getElementById('historic').innerText;
@@ -10,3 +12,21 @@ function tweetIt () {
       
     window.open(tweetUrl);
 }
+
+// second attempt
+
+// update when map clicked
+
+$('#map').on('click', function(ev) {
+    ev.preventDefault();
+    // Remove existing iframe
+    $('#tweetBtn iframe').remove();
+    // Generate new markup
+    var tweetBtn = $('<a></a>')
+        .addClass('twitter-share-button')
+        .attr('href', 'http://twitter.com/share')
+        .attr('data-url', 'http://test.com')
+        .attr('data-text', $('#historic').text());
+    $('#tweetBtn').append(tweetBtn);
+    twttr.widgets.load();
+});
