cbc6993d20c51d41c890b9f9dc6bde0332605630
Rosamund Pearce <rosamundpearce@live.com>
2018-08-07 12:14:03 +0100

    Opted to load data as JS object for now

--- js/text.js
+++ js/text.js
@@ -1,10 +1,10 @@
-var city = {
-    // create array from the data that Zeke sends?
-    // or save as JSON?
-    // saving it as a json triples the file size, so will see whether can use this 
-    // jquery library to read the CSV
-    1: "Lat: 89.5, Long: 150.5, over the ocean"
-}
+// var city = {
+//     // create array from the data that Zeke sends?
+//     // or save as JSON?
+//     // saving it as a json triples the file size, so will see whether can use this 
+//     // jquery library to read the CSV
+//     1: "Lat: 89.5, Long: 150.5, over the ocean"
+// }
 
 // document.getElementById('city').innerText = city
 
@@ -14,3 +14,4 @@ var city = {
 // var characteristics = $.csv.toObjects(textCsv);
 
 console.log(characteristics[0]);
+
