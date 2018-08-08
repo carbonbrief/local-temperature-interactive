5f36d30c001383c623ad6538b463069ba2a7b73f
Rosamund Pearce <rosamundpearce@live.com>
2018-08-08 14:31:59 +0100

    add city names

--- js/text.js
+++ js/text.js
@@ -2,20 +2,26 @@
 
 document.getElementById('map').addEventListener("click", function () {
 
-    var coords = midCoordLat + ", " + midCoordLong;
+    // load characteristics data
+    var script = document.createElement('script');
+    script.src = "characteristics.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    var coords = midCoordLat + "," + midCoordLong;
 
     // create new array from characteristics.js based on click event
     var newArray = characteristics.filter(function(x) {
-        return x.place === coords;
+        return x.geo === coords;
     });
 
-    var country = (newArray[0]["country"]); 
+    var country = (newArray[0]["adm"]); 
     var observed = (newArray[0]["obs"]);
     var rcp26 = (newArray[0]["26"]);
     var rcp85 = (newArray[0]["85"]);
+    var city = (newArray[0]["city"]);
 
     document.getElementById('country').innerText = country;
-    document.getElementById('city').innerText = coords;
+    document.getElementById('city').innerText = city;
     document.getElementById('warming').innerText = observed;
     document.getElementById('future-warming1').innerText = rcp26;
     document.getElementById('future-warming2').innerText = rcp85;
